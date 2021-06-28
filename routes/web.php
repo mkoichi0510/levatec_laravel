c755ad8f2a663ea684b45fadce1489ffa88e744c
mkoichi0510 <mkoichi0510@gmail.com>
2021-06-28 23:49:42 +0900

    [add]ブログの詳細ページの追加とそれらのページを行き来するリンクの追加

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,11 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+*/
+Route::get('/', 'PostController@index');
+
+Route::get('/posts/{post}', 'PostController@show');

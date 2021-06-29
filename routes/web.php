e9e8b0c6c1767ae6c39c98f7920b792b35f340af
mkoichi0510 <mkoichi0510@gmail.com>
2021-06-29 14:41:41 +0900

    [add]ブログの新規作成ページと追加内容をDBに登録する処理の追加

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,7 @@ Route::get('/', function () {
 */
 
 Route::get('/', 'PostController@index');
-
+Route::get('/posts/create', 'PostController@create');
 Route::get('/posts/{post}', 'PostController@show');
+
+Route::post('/posts', 'PostController@store');
